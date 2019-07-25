584177975e51ef58f8141a3aca7cf53e72559593
zhanghonggang <395372289@qq.com>
2019-07-25 17:37:21 +0800

    add extend_friend_syntax

---
+++ src/extend_friend_syntax/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T> class DefenderT {
+public:
+    friend T;
+    void Defence(int x, int y) {}
+    void Tackle(int x, int y) {}
+
+private:
+    int pos_x = 15;
+    int pos_y = 0;
+    int speed = 2;
+    int stamina = 120;
+};
+
+template <typename T> class AttackerT {
+public:
+    friend T;
+    void Move(int x, int y) {}
+    void SpeedUp(float ratio) {}
+
+private:
+    int pos_x = 0;
+    int pos_y = -30;
+    int speed = 3;
+    int stamina = 100;
+};
+
+using Defender = DefenderT<int>;
+using Attacker = AttackerT<int>;
+
+int main()
+{
+    cout <<"123" << endl;
+    return 0;
+}
+#ifdef UNIT_TEST
+class Validator {
+public:
+    void Validate (int x, int y, DefenderTest& d) {}
+    void Validate (int x, int y, AttackerTest& a) {}
+};
+
+using DefenderTest = DefenderT<Validator>;
+using AttackerTest = AttackerT<Validator>;
+
+int main(){
+    cout << "Hello World!" << endl;
+    return 0;
+}
+#endif
